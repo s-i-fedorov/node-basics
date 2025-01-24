a1f7355be804e59fdd6676cba7822fe414de191a
s-i-fedorov <s.i.fedorov.87@gmail.com>
2025-01-24 14:46:13 +0200

    hw_22 start

--- app.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('data.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error("Помилка читання файлу:", err);
-        return;
-    }
-    console.log("Вміст файлу data.txt:", data);
-});
