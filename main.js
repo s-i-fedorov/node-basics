78099c488288cba4ed800a91ed42e7e17ffb693a
s-i-fedorov <s.i.fedorov.87@gmail.com>
2025-01-25 11:29:51 +0200

    almost done

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+app.use((req,res,next)=>{
+    console.log(`Method: ${req.method},\n URL: ${req.url}`);
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send('Hello from Express.js');
 });
@@ -10,7 +17,23 @@ app.get('/user/:id', (req, res) => {
     res.send(`You asked user with id = ${userId}`);
 });
 
+app.post('/submit', (req, res) => {
+    const { name, email } = req.body;
+    res.send(`Отримано дані: Ім'я - ${name},\n 
+        Електронна пошта - ${email}`);
+});
+
+app.use((req,res,next)=>{
+    res.status(404).send('Page not found');
+    next()
+})
+
+app.use((req,res,next)=>{
+    res.status(500).send('Something was wrong');
+    next()
+})
 
 app.listen(3000, () => {
     console.log('Сервер працює на порту 3000');
 });
+
